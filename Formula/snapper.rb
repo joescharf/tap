9169945f6f921baac8da03082f139dc55ace5a5c
releaserbot <info@dbsnapper.com>
2021-02-03 13:43:23 -0700

    Brew formula update for dbsnapper-cli version v0.3.0

--- Formula/snapper.rb
+++ Formula/snapper.rb
@@ -5,16 +5,16 @@
 class Snapper < Formula
   desc "Create sanitized development snapshots of your production databases."
   homepage "https://dbsnapper.com/"
-  version "0.2.0"
+  version "0.3.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/joescharf/snapper/releases/download/v0.2.0/dbsnapper-cli_snapper_0.2.0_Darwin_x86_64.zip"
-    sha256 "93f81393beec450daf011459f417e6e52825fe2f9ef78a0ca1e8fcdcb9628c59"
+    url "https://github.com/joescharf/snapper/releases/download/v0.3.0/dbsnapper-cli_snapper_0.3.0_Darwin_x86_64.zip"
+    sha256 "e5b36cc54c9338dc7b23a1dd3f7fee868b7f0ec31f7b2f320ca2a3c33c09a114"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/joescharf/snapper/releases/download/v0.2.0/dbsnapper-cli_snapper_0.2.0_Linux_x86_64.zip"
-    sha256 "da1b1f1e6723b6eb9331c7330b5f70ad994ae258ae0d8a30f95ebd6094d74cfd"
+    url "https://github.com/joescharf/snapper/releases/download/v0.3.0/dbsnapper-cli_snapper_0.3.0_Linux_x86_64.zip"
+    sha256 "384529ccd0c4632baa93e8ccf86e6489179aea672c380f50195f54182a3cd3a0"
   end
 
   def install
