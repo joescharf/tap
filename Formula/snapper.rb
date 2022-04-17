93fffde74c8d32a33ae7503921265433b36e0921
releaserbot <info@dbsnapper.com>
2022-04-17 08:26:14 -0600

    Brew formula update for dbsnapper version v0.9.2

--- Formula/snapper.rb
+++ Formula/snapper.rb
@@ -5,19 +5,33 @@
 class Snapper < Formula
   desc "Create sanitized development snapshots of your production databases."
   homepage "https://dbsnapper.com/"
-  version "0.9.1"
-  bottle :unneeded
+  version "0.9.2"
 
-  if OS.mac?
-    url "https://github.com/joescharf/snapper/releases/download/v0.9.1/dbsnapper-cli_snapper_0.9.1_Darwin_x86_64.zip"
-    sha256 "f121f5dfbf700185072c885c1e53d553c279bbfe56db08876189c73ea0061d27"
-  end
-  if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/joescharf/snapper/releases/download/v0.9.1/dbsnapper-cli_snapper_0.9.1_Linux_x86_64.zip"
-    sha256 "873f1f8b12d1b76c9a342acac4cafa83eaad8f5b2dceac2b1a66a8a17ae4c489"
+  on_macos do
+    url "https://github.com/joescharf/snapper/releases/download/v0.9.2/dbsnapper_snapper_0.9.2_Darwin_all.zip"
+    sha256 "16f0c1dea30f5da9a484af9fb05b79f70639b8780e2696ac90af4f94d72046c8"
+
+    def install
+      bin.install "snapper"
+    end
   end
 
-  def install
-    bin.install "snapper"
+  on_linux do
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/joescharf/snapper/releases/download/v0.9.2/dbsnapper_snapper_0.9.2_Linux_arm64.zip"
+      sha256 "dcf0ecb704a149a1be916bfb877132d23a7ee901b55cf437145d3a44e3190510"
+
+      def install
+        bin.install "snapper"
+      end
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/joescharf/snapper/releases/download/v0.9.2/dbsnapper_snapper_0.9.2_Linux_x86_64.zip"
+      sha256 "9dbc69968a9af7e14729c49e9c92cc2e4fc2446def33309b1d5d2338a7ec26d0"
+
+      def install
+        bin.install "snapper"
+      end
+    end
   end
 end
